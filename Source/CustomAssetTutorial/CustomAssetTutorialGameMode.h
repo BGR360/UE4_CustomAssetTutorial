4e63f82a80ae65a699d6fe9c74b58cddeb723197
Ben Reeves <benwolverine2019@gmail.com>
2015-12-02 19:16:42 -0500

    Initial Commit

---
+++ Source/CustomAssetTutorial/CustomAssetTutorialGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "CustomAssetTutorialGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CUSTOMASSETTUTORIAL_API ACustomAssetTutorialGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
